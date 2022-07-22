0bf7a4a755b9b9e76ec8e23ca17939bffb8ca768
Paulo Marvin <paulo.marving@gmail.com>
2022-07-21 21:52:50 -0300

    adiciona notebooks em suas respectivas abas

--- src/app/analise_comparativa.py
+++ src/app/analise_comparativa.py
@@ -4,7 +4,7 @@ from streamlit.components import v1 as components
 
 def page():
     st.title('Análise comparativa')
-    # plot_file = open('comparative_analysis.html', 'r')
-    # plot = plot_file.read()
-    # components.html(plot, width=900, height=700, scrolling=True)
-    # plot_file.close()
+    plot_file = open('src/app/comparative_analysis.html', 'r')
+    plot = plot_file.read()
+    components.html(plot, width=900, height=700, scrolling=True)
+    plot_file.close()
--- src/app/analise_exploratoria.py
+++ src/app/analise_exploratoria.py
@@ -4,8 +4,8 @@ from streamlit.components import v1 as components
 
 def page():
     st.title('Análise exploratória')
-    # plot_file = open('exploratory_data_analysis.html', 'r')
-    # plot = plot_file.read()
-    # components.html(plot, width=900, height=700, scrolling=True)
-    # plot_file.close()
+    plot_file = open('src/app/exploratory_data_analysis.html', 'r')
+    plot = plot_file.read()
+    components.html(plot, width=900, height=700, scrolling=True)
+    plot_file.close()
     
